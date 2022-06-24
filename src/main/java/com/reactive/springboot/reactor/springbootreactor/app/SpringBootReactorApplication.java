0350e3a4929f252a76796f6969afaabf9595466c
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2022-06-24 02:42:28 -0500

    añadir log a la peticion

--- src/main/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplication.java
@@ -1,12 +1,17 @@
 package com.reactive.springboot.reactor.springbootreactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+	
+	private  static  final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -23,7 +28,8 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 		//Cada vez que recibimos que nos notifique
 		
 		//Nos subscribismo
-		nombres.subscribe();
+		//nombres.subscribe(e -> log.info(e));
+		nombres.subscribe(log::info);
 		
 		
 		
