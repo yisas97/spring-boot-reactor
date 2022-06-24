fea3187e489a1ffcd4afa14a649314ac7dac4326
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2022-06-24 02:21:30 -0500

    Init of projects

---
+++ src/main/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.springboot.reactor.springbootreactor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.reactive.springboot.reactor.springbootreactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+	
+	@Test
+	void contextLoads() {
+		//Example
+	}
+	
+}
