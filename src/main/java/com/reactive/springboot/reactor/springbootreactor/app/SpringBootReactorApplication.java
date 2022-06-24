5b95614c4025eb64968662d7d8cdfbd02ecd852a
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2022-06-24 02:34:31 -0500

    Create a stream flux observable

--- src/main/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/reactive/springboot/reactor/springbootreactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,32 @@
 package com.reactive.springboot.reactor.springbootreactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 	
+	@Override
+	public void run(String... args) throws Exception {
+		// Metodo Just creamos nuevos flujo de datos
+		//Flux<String> nombres = Flux.just("Jesus","Junior","Carlos","Genaro","Cristhian", "Andres")
+		//		.doOnNext(e -> System.out.println(e));
+		//Factorizado
+		Flux<String> nombres = Flux.just("Jesus","Junior","Carlos","Genaro","Cristhian", "Andres")
+				.doOnNext(System.out::println);
+		//Cada vez que recibimos que nos notifique
+		
+		//Nos subscribismo
+		nombres.subscribe();
+		
+		
+		
+		
+	}
 }
